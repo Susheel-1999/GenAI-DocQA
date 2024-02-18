deffff7831dddc3e024c972de79e06bd3036648b
Susheel-1999 <susheelnagesh@gmail.com>
2024-02-18 12:50:55 +0530

    updated the model and used huggingface hub from langchain

--- app.py
+++ app.py
@@ -6,27 +6,26 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.schema import Document
 from langchain.prompts import PromptTemplate
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.chains import LLMChain
+from langchain_community.llms import HuggingFaceHub
 import os
-os.environ['CURL_CA_BUNDLE'] = ''
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = ''
 
-llm = HuggingFacePipeline.from_model_id(
-    model_id="gpt2",
-    task="text-generation",
-    pipeline_kwargs={"max_new_tokens": 30})
+llm = HuggingFaceHub(
+    repo_id="google/flan-t5-xxl", 
+    model_kwargs={"temperature": 0.5, "max_length": 64})
 
 template = """
-{context} 
-
-Answer the give question with the above context
+Try to answer the Question based on the Context.
+Context: {context}
 Question: {question}
-Answer: """
+Answer:"""
+
 prompt = PromptTemplate.from_template(template)
-chain = prompt | llm
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-@st.cache_data
 def read_pdf(file_contents):
     try:
         reader = PdfReader(file_contents)
@@ -35,10 +34,9 @@ def read_pdf(file_contents):
         pdf_texts = "\n\n".join(pdf_texts)
         return pdf_texts
     except Exception as e:
-        print("Read PDF -",e)
+        print("Error faced in Read PDF -",e)
         return ""
 
-@st.cache_data
 def chunking(doc_text):
     try:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size = 200, chunk_overlap = 10,
@@ -46,10 +44,9 @@ def chunking(doc_text):
         chunks = text_splitter.split_text(doc_text)
         return chunks
     except Exception as e:
-        prnt("Chunking -",e)
+        prnt("Error faced in Chunking -",e)
         return doc_text
 
-@st.cache_data
 def vectorize_text(pdf_chunks):
     with st.spinner("Indexing into DB..."):
         return embedding_model.embed_documents(pdf_chunks)
@@ -74,10 +71,7 @@ def main():
 
         # Collapsible section for Preview
         with st.expander("click here to see the document content", expanded=False):
-            # Display PDF content in a text area
             st.text_area("Document Content Preview", pdf_texts, height=400)
-
-        print("text", pdf_texts)
 
         # Chunking
         with st.spinner("Chunking..."):
@@ -95,10 +89,8 @@ def main():
 
         # Fetch Answer Button
         if st.button("Find Answer"):
-            # Perform operations with the entered text (replace with your logic)
-            # For demonstration, simply displaying the entered query
             with st.spinner("Generating..."):
-                st.success(chain.invoke({"question": question, "context":topn}))
+                st.success(llm_chain.run({"question": user_query, "context":topn}))
 
 if __name__ == '__main__':
     main()
